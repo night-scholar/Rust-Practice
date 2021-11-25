6333f994e54bc99f9a18d3dc7bea2c18fca68fd8
CK <84382448+night-scholar@users.noreply.github.com>
2021-11-25 17:05:20 +0800

    Delete channel/target directory

--- channel/target/rls/debug/deps/channel-e4c3b7735be1edd3.d
+++
@@ -1,5 +0,0 @@
-/home/yehee/ck2/rust/channel/target/rls/debug/deps/channel-e4c3b7735be1edd3.rmeta: src/main.rs
-
-/home/yehee/ck2/rust/channel/target/rls/debug/deps/channel-e4c3b7735be1edd3.d: src/main.rs
-
-src/main.rs:
